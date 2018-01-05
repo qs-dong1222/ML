33faad6ba974e823de4c5d4ded945551c5e37efe
Q <shi.qiu.1222@gmail.com>
2018-01-05 22:45:30 +0800

    LR
    LR

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -1,46 +1,11 @@
 import numpy as np
+from Batch import *
 
-
-
-# class Batch:
-#     def __init__(self, featureSet, labelSet):
-#         if (featureSet.shape[0] != labelSet.shape[0]):
-#             print("featureSet number != labelSet number");
-#             print("featureSet size, ", featureSet.shape)
-#             print("labelSet size, ", labelSet.shape)
-#             return None;
-#         else:
-#             self.featureSet = featureSet;
-#             self.labelSet = labelSet;
-#             self.endIdx = self.featureSet.shape[0] - 1;
-#             self.currIdx = 0;
-#
-#     def next_batch(self, batchNbr, sequentialBatch=True):
-#         if (batchNbr > self.featureSet.shape[0]):
-#             batchNbr = self.featureSet.shape[0]
-#
-#         if (sequentialBatch):
-#             lastIdx = self.currIdx + batchNbr - 1;
-#             if (lastIdx < self.endIdx):
-#                 retFeatureBatch = self.featureSet[self.currIdx:lastIdx + 1];
-#                 retLabelBatch = self.labelSet[self.currIdx:lastIdx + 1];
-#                 self.currIdx = lastIdx + 1;
-#                 return retFeatureBatch, retLabelBatch;
-#             elif (lastIdx == self.endIdx):
-#                 retFeatureBatch = self.featureSet[self.currIdx:lastIdx + 1];
-#                 retLabelBatch = self.labelSet[self.currIdx:lastIdx + 1];
-#                 self.currIdx = 0;
-#                 return retFeatureBatch, retLabelBatch;
-#             else:
-#                 retFeatureBatch = self.featureSet[self.currIdx:self.endIdx + 1];
-#                 retLabelBatch = self.labelSet[self.currIdx:self.endIdx + 1];
-#                 self.currIdx = 0;
-#                 return retFeatureBatch, retLabelBatch;
-#         else:
-#             idxes = np.random.randint(0, self.endIdx + 1, batchNbr);
-#             retFeatureBatch = self.featureSet[idxes, :];
-#             retLabelBatch = self.labelSet[idxes, :];
-#             return retFeatureBatch, retLabelBatch;
+def numpilize(array_x):
+    x = np.array(array_x)
+    if (x.ndim < 2):
+        x = x.reshape(1, x.shape[0])
+    return x
 
 
 
@@ -50,16 +15,12 @@ class LinearRegressionOptimizer:
 
 
     def __LoadData__(self, featureSet, labelSet):
-        self.xdata = np.array(featureSet)
-        if(self.xdata.ndim<2):
-            self.xdata = self.xdata.reshape(1, self.xdata.shape[0])
-        self.ydata = np.array(labelSet)
-        if (self.ydata.ndim < 2):
-            self.ydata = self.ydata.reshape(1, self.ydata.shape[0])
+        self.xdata = numpilize(featureSet)
+        self.ydata = numpilize(labelSet)
         n_feature = self.xdata.shape[1] # number of cols in xdata
         initOffset = np.random.randint(low=0,high=10,size=n_feature) # randint 0~9
         self.w = np.random.randn(n_feature) + initOffset # randint 0~9 + fraction[0~1]
-        self.w = self.w.reshape(1,n_feature)
+        self.w = numpilize(self.w)
         self.b = np.random.randint(0,10);
 
 
@@ -68,11 +29,14 @@ class LinearRegressionOptimizer:
 
 
     def fit(self, learningRate=0.02, epoch=2000, batchSize=100, regLambda=0, showLoss=False):
-        #batchSet_XY = Batch(self.xdata, self.ydata);
+        self.deriv_w_sqsum = np.zeros([1, self.xdata.shape[1]])
+        self.deriv_b_sqsum = 0
+        self.ada_iter = 0
+
+        batchSet_XY = Batchor(self.xdata, self.ydata);
         for i in range(epoch):
-            #batch_xs, batch_ys = batchSet_XY.next_batch(batchSize);
-            #self.Train(batch_xs, batch_ys, learningRate, regLambda)
-            self.Train(self.xdata, self.ydata, learningRate, regLambda)
+            batch_xs, batch_ys = batchSet_XY.NextBatch(batchSize);
+            self.Train(batch_xs, batch_ys, learningRate, regLambda)
             if (i%50==0 and showLoss):
                 print(self.GetLoss(self.xdata, self.ydata))
 
@@ -93,22 +57,30 @@ class LinearRegressionOptimizer:
 
 
     def Train(self, featureSet, labelSet, learningRate=0.02, regLambda=0):
+        ######################
+        ### Adagrad method ###
+        ######################
         self.regArg = regLambda
         error = labelSet.T - self.predict(featureSet)
+        self.ada_iter += 1
 
         deriv_w = np.mean(featureSet * (-1) * error, axis=0)
-        self.w = (1-self.regArg)*self.w - learningRate*deriv_w
+        self.deriv_w_sqsum  += np.square(deriv_w)
+        ada_w = np.sqrt(self.deriv_w_sqsum / self.ada_iter)
+        ada_w = np.where(ada_w == 0, 0e-7, ada_w)
+        self.w = (1-self.regArg)*self.w - (learningRate/ada_w)*deriv_w
+
         deriv_b = np.mean((-1) * error, axis=0)
-        self.b = self.b - learningRate*deriv_b
+        self.deriv_b_sqsum += np.square(deriv_b)
+        ada_b = np.sqrt(self.deriv_b_sqsum / self.ada_iter)
+        ada_b = np.where(ada_b == 0, 0e-7, ada_b)
+        self.b = self.b - (learningRate/ada_b)*deriv_b
 
 
     def GetAccuracy(self, featureSet, labelSet):
-        x = np.array(featureSet)
-        if(x.ndim<2):
-            x = x.reshape(1, x.shape[0])
-        y = np.array(labelSet)
-        if(y.ndim < 2):
-            y = y.reshape(1, y.shape[0])
+        x = numpilize(featureSet)
+        y = numpilize(labelSet)
+        y = np.where(y==0, 0e-7, y)
         error = y.T - self.predict(featureSet)
         acc = (1 - np.max(np.abs(error / y.T), axis=0)) * 100
         return acc
@@ -122,20 +94,21 @@ class LinearRegressionOptimizer:
 
 
 
-x = np.random.randint(10,size=200).reshape(100,2)
-y = [i for i in range(1,21)]
-y = y*5
+# x = np.random.randint(10,size=200).reshape(100,2)
+# y = [i for i in range(1,21)]
+# y = y*5
 
-x = np.arange(1,51).reshape(10,5)
-y = [10,20,30,40,50,60,70,80,90,100]
 
-lr = LinearRegressionOptimizer(x, y)
-lr.fit(0.0001, 18000)
 
-print("acc = ",lr.GetAccuracy(x,y))
 
-print("ans = \n", lr.predict(x))
-
+# x = np.arange(1,51).reshape(10,5)
+# y = [10,20,30,40,50,60,70,80,90,100]
+#
+# lr = LinearRegressionOptimizer(x, y)
+# lr.fit(0.3, 2000, showLoss=True)
+#
+# print("acc = ",lr.GetAccuracy(x,y))
+# print("ans = \n", lr.predict(x))
 
 
 
