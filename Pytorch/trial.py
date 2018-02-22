d481c1eaa45b72f8575bd0ce40a1c47014ed1818
stonewinter <shi.qiu.1222@gmail.com>
2018-02-22 10:42:12 +0800

    deconv trial
    deconv trial

--- Pytorch/trial.py
+++ Pytorch/trial.py
@@ -1,42 +1,91 @@
 import torch as t
 from torch.autograd import Variable
+import cv2
+import opencvlib
 
 
-# class Model(t.nn.Module):
-#     def __init__(self):
-#         super(Model, self).__init__()
-#         self.m1 = t.nn.Sequential(
-#             t.nn.Linear(10, 15),
-#             t.nn.BatchNorm1d(15),
-#             t.nn.ReLU(),
-#             t.nn.Linear(15, 20),
-#             t.nn.BatchNorm1d(20),
-#             t.nn.ReLU(),
-#             t.nn.Linear(20, 5)
-#         )
-#
-#     def forward(self, input):
-#         output = self.m1(input)
-#         return output
-#
-#
-# model = Model().cuda()
-# print(model)
-#
-#
-#
-#
-#
-# x = Variable(t.randn(1000, 10)).cuda()
-#
-# o = model(x)
-# print(o)
+
+# hyper params
+K_SIZE = 15
+DE_K_SIZE = 40
+
+
+
+class ConvModel(t.nn.Module):
+    def __init__(self):
+        super(ConvModel, self).__init__()
+        self.m1 = t.nn.Sequential(
+            t.nn.Conv2d(1, 1, (K_SIZE, K_SIZE))
+        )
+
+    def forward(self, input):
+        output = self.m1(input)
+        return output
 
 
 
 
 
+class DeconvModel(t.nn.Module):
+    def __init__(self):
+        super(DeconvModel, self).__init__()
+        self.m = t.nn.Sequential(
+            t.nn.ConvTranspose2d(1, 1, (DE_K_SIZE, DE_K_SIZE))
+        )
 
-x = t.randn(5)
-vx = Variable(x).cuda().view(-1, 5)
-print(vx)
+    def forward(self, input):
+        output = self.m(input)
+        return output
+
+
+
+
+
+
+
+
+
+
+convmodel = ConvModel()
+deconvmodel = DeconvModel()
+
+
+
+
+
+
+img = cv2.imread("./leona.jpg", 0).reshape(1, 1, 512, 512)
+
+x = Variable(t.FloatTensor(img))
+out = convmodel(x)
+
+out_size = ((512 - K_SIZE)//1) + 1
+outimg = out.data.numpy().reshape(out_size, out_size)
+cv2.imshow("conv img", outimg)
+print("conv img size: ", outimg.shape)
+
+
+
+deconv_out = deconvmodel(out)
+deconvout_size = (out_size-1)*1 + DE_K_SIZE
+deoutimg = deconv_out.data.numpy().reshape(deconvout_size, deconvout_size)
+
+cv2.imshow("deconv img", deoutimg)
+print("deconv img size: ", deoutimg.shape)
+
+
+
+
+
+
+
+
+opencvlib.WaitEscToExit()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
