9fdfc7123b5365aa02b39dd7cceaecc202b45b1f
stonewinter <shi.qiu.1222@gmail.com>
2018-02-24 14:12:13 +0800

    DCGAN
    DCGAN

--- Pytorch/trial.py
+++ Pytorch/trial.py
@@ -1,87 +1,32 @@
-import torch as t
-from torch.autograd import Variable
-import cv2
-import opencvlib
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 
-# hyper params
-K_SIZE = 15
-DE_K_SIZE = 40
+# data preparation
+train_images = np.load("./MNIST/npy-format-data/train_images.npy")/255
+train_labels = np.load("./MNIST/npy-format-data/train_labels.npy")
+test_images = np.load("./MNIST/npy-format-data/test_images.npy")/255
+test_labels = np.load("./MNIST/npy-format-data/test_labels.npy")
+
+
+idxs = np.where(train_labels==8)
+train_images = train_images[idxs]
+train_labels = train_labels[idxs]
 
 
 
-class ConvModel(t.nn.Module):
-    def __init__(self):
-        super(ConvModel, self).__init__()
-        self.m1 = t.nn.Sequential(
-            t.nn.Conv2d(1, 1, (K_SIZE, K_SIZE))
-        )
-
-    def forward(self, input):
-        output = self.m1(input)
-        return output
+col_size = 5
+grid_imgs = np.vstack([ np.hstack([img for img in train_images[s:s+col_size]]) for s in range(0, col_size*5, col_size)])
 
 
-
-
-
-class DeconvModel(t.nn.Module):
-    def __init__(self):
-        super(DeconvModel, self).__init__()
-        self.m = t.nn.Sequential(
-            t.nn.ConvTranspose2d(1, 1, (DE_K_SIZE, DE_K_SIZE))
-        )
-
-    def forward(self, input):
-        output = self.m(input)
-        return output
+plt.imshow(grid_imgs, cmap='gray')
+plt.show()
 
 
 
 
 
-
-
-
-
-
-convmodel = ConvModel()
-deconvmodel = DeconvModel()
-
-
-
-
-
-
-img = cv2.imread("./leona.jpg", 0).reshape(1, 1, 512, 512)
-
-x = Variable(t.FloatTensor(img))
-out = convmodel(x)
-
-out_size = ((512 - K_SIZE)//1) + 1
-outimg = out.data.numpy().reshape(out_size, out_size)
-cv2.imshow("conv img", outimg)
-print("conv img size: ", outimg.shape)
-
-
-
-deconv_out = deconvmodel(out)
-deconvout_size = (out_size-1)*1 + DE_K_SIZE
-deoutimg = deconv_out.data.numpy().reshape(deconvout_size, deconvout_size)
-
-cv2.imshow("deconv img", deoutimg)
-print("deconv img size: ", deoutimg.shape)
-
-
-
-
-
-
-
-
-opencvlib.WaitEscToExit()
-cv2.destroyAllWindows()
 
 
 
