65d00a91cf0daa8ef4f0d4a20cb63142f44ac8b7
stonewinter <shi.qiu.1222@gmail.com>
2018-03-04 23:25:05 +0800

    CNN
    CNN

--- Pytorch/my_note_CNN.py
+++ Pytorch/my_note_CNN.py
@@ -56,7 +56,7 @@ import torch.utils.data as utdata
 train_set_x = t.FloatTensor(train_set_x)/255
 train_set_y = t.LongTensor(train_set_y)
 dataSet = utdata.TensorDataset(data_tensor=train_set_x, target_tensor=train_set_y)
-dataLoader = utdata.DataLoader(dataSet, batch_size=BATCH_SIZE, shuffle=True)
+dataLoader = utdata.DataLoader(dataSet, batch_size=BATCH_SIZE, shuffle=True, num_workers=1)
 
 test_set_x = t.FloatTensor(test_set_x)/255
 
@@ -112,68 +112,51 @@ class FullConnect(t.nn.Module):
 
 
 
+if __name__ == '__main__':
+    conv_model = Conv().cuda()
+    fc_model = FullConnect().cuda()
+
+    # loss + cuda
+    loss_func = t.nn.NLLLoss().cuda()
+
+    # optimizer
+    opt_conv = t.optim.Adam(conv_model.parameters(), lr=LR)
+    opt_fc = t.optim.Adamax(fc_model.parameters(), lr=LR)
+
+    for eopch in range(EPOCH):
+        for batch_Nbr, (batch_xs, batch_ys) in enumerate(dataLoader):
+            batch_xs_cuda = Variable(batch_xs).cuda()
+            batch_ys_cuda = Variable(batch_ys).cuda()
+
+            opt_conv.zero_grad()
+            opt_fc.zero_grad()
+            y_pred = conv_model(batch_xs_cuda)
+            y_pred = y_pred.view(-1, 64*4*4)
+            y_pred = fc_model(y_pred)
+
+            loss = loss_func(y_pred, batch_ys_cuda)
+
+            if (batch_Nbr % (BATCH_SIZE/2) == 0):
+                print("loss = %.4f" % (loss.data.cpu()[0]))
+                predition = conv_model(Variable(test_set_x[0:1000]).cuda())
+                predition = predition.view(-1, 64 * 4 * 4)
+                predition = fc_model(predition)
+                predition = predition.cpu().data.numpy()
+                pred_labels = np.argmax(predition, axis=1)
+                true_labels = test_set_y[0:1000]
+                acc = np.sum(pred_labels == true_labels) / 1000
+                print("acc = %.4f" % acc)
+
+            loss.backward()
+            opt_conv.step()
+            opt_fc.step()
 
 
 
-conv_model = Conv().cuda()
-fc_model = FullConnect().cuda()
-
-
-
-
-
-
-
-# loss + cuda
-loss_func = t.nn.NLLLoss().cuda()
-
-# optimizer
-opt_conv = t.optim.Adam(conv_model.parameters(), lr=LR)
-opt_fc = t.optim.Adamax(fc_model.parameters(), lr=LR)
-
-
-
-
-
-
-
-
-
-
-for eopch in range(EPOCH):
-    for batch_Nbr, (batch_xs, batch_ys) in enumerate(dataLoader):
-        batch_xs_cuda = Variable(batch_xs).cuda()
-        batch_ys_cuda = Variable(batch_ys).cuda()
-
-        opt_conv.zero_grad()
-        opt_fc.zero_grad()
-        y_pred = conv_model(batch_xs_cuda)
-        y_pred = y_pred.view(-1, 64*4*4)
-        y_pred = fc_model(y_pred)
-
-        loss = loss_func(y_pred, batch_ys_cuda)
-
-        if (batch_Nbr % (BATCH_SIZE/2) == 0):
-            print("loss = %.4f" % (loss.data.cpu()[0]))
-            predition = conv_model(Variable(test_set_x[0:1000]).cuda())
-            predition = predition.view(-1, 64 * 4 * 4)
-            predition = fc_model(predition)
-            predition = predition.cpu().data.numpy()
-            pred_labels = np.argmax(predition, axis=1)
-            true_labels = test_set_y[0:1000]
-            acc = np.sum(pred_labels == true_labels) / 1000
-            print("acc = %.4f" % acc)
-
-        loss.backward()
-        opt_conv.step()
-        opt_fc.step()
-
-
-
-y_pred = conv_model(Variable(train_set_x[0:2]).cuda())
-y_pred = y_pred.view(-1, 64*4*4)
-y_pred = fc_model(y_pred)
-print(y_pred)
-print(train_set_y[0:2])
+    y_pred = conv_model(Variable(train_set_x[0:2]).cuda())
+    y_pred = y_pred.view(-1, 64*4*4)
+    y_pred = fc_model(y_pred)
+    print(y_pred)
+    print(train_set_y[0:2])
 
 
